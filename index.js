853ff36fb7b17b8c8a8803d4bda1dc7f3ffe15e5
AmirZapam <amirzapam9@gmail.com>
2023-12-07 19:41:23 +0330

    finished + one bug

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
 let button = document.querySelector(".add");
+let toDoList = document.querySelector(".todo-list");
 
 button.addEventListener("click", () => {
 	const input = document.getElementById("input-box");
 	const todo = input.value;
-  let error = document.querySelector(".invalid-input")
+	let error = document.querySelector(".invalid-input");
 
-	if (todo !== '') {
-    let toDoList = document.querySelector(".todo-list");
-	let newToDo = document.createElement("li");
+	if (todo !== "") {
+		let newToDoDiv = document.createElement("div");
+		let newToDo = document.createElement("li");
+		let deleteButton = document.createElement("button");
+		let deleteIcon = document.createElement("li");
+		deleteIcon.classList.add(
+			"bi",
+			"bi-x",
+			"text-red-500",
+			"text-4xl",
+			"hover:scale-150",
+			"transition-all",
+			"duration-500"
+		);
+		let checkBox = document.createElement("input");
+		checkBox.type = "checkbox";
 
-	let checkBox = document.createElement("input");
-	checkBox.type = "checkbox";
+		let todoText = document.createTextNode(todo);
 
-	let todoText = document.createTextNode(todo);
+		newToDo.appendChild(checkBox);
+		newToDo.appendChild(todoText);
+		newToDoDiv.appendChild(newToDo);
+		toDoList.appendChild(newToDoDiv);
+		deleteButton.appendChild(deleteIcon);
+		newToDoDiv.appendChild(deleteButton);
 
-	newToDo.appendChild(checkBox);
-	newToDo.appendChild(todoText);
-	toDoList.appendChild(newToDo);
-  checkBox.classList.add("mr-2");
-	newToDo.classList.add("flex", "items-center", 'pt-5',);
-  error.classList.remove("hidden");
-  error.classList.add("hidden");
+		checkBox.classList.add(
+			"form-checkbox",
+			"text-indigo-600",
+			"h-5",
+			"w-5",
+			"mr-2",
+			"rounded-full"
+		);
+		newToDo.classList.add("flex", "items-center");
+		error.classList.remove("hidden");
+		error.classList.add("hidden");
+		newToDoDiv.classList.add(
+			"flex",
+			"justify-between",
+			"items-center",
+			"py-4",
+			"px-4",
+			"border-gray-200"
+		);
+		saveData();
 
-	console.log(todo);
-	input.value = "";
-  }else{
-    error.classList.remove("hidden");
-  }
+		deleteButton.addEventListener("click", () => {
+			toDoList.removeChild(newToDoDiv);
+			saveData(); // Update local storage after deleting a task
+		});
+
+		console.log(todo);
+		input.value = "";
+	} else {
+		error.classList.remove("hidden");
+	}
 });
+
+function saveData() {
+	localStorage.setItem("data", toDoList.innerHTML);
+}
+
+function ShowTask() {
+	toDoList.innerHTML = localStorage.getItem("data");
+}
+ShowTask();
